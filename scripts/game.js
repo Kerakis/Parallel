a8b367641de75c3a17e963cd0f63ca9608c8d832
Rob Upchurch <rlupchurch3@gmail.com>
2022-02-28 16:29:33 -0500

    Preliminary modal added for difficulty setting

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,28 @@ let numberAttempted = 0;
 let cardName = "";
 let cardArt = "";
 
+function modal() {
+  // Get the modal
+var modal = document.getElementById("diffModal");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
+
 function init() {
   // Submit button for user input
   const submitButton = document.querySelector(`#submitButton`);
