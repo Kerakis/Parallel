2ce07bcd1546153437aede67759eec31a5720039
Rob Upchurch <rlupchurch3@gmail.com>
2022-12-16 16:17:04 -0500

    Create asset directory if it doesn't exist

--- update-card-catalog.sh
+++ update-card-catalog.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+mkdir -p assets
 url=$(curl https://api.scryfall.com/bulk-data | jq -r '.data[] | select(.type=="oracle_cards").download_uri')
 "$url"
 curl -o assets/data.json "$url"
-jq -s -c 'map(.[] | select(.legalities.standard=="legal") | .name)' assets/data.json > assets/standard.json
-jq -s -c 'map(.[] | select(.legalities.pauper=="legal") | .name)' assets/data.json > assets/pauper.json
-jq -s -c 'map(.[] | select(.legalities.pioneer=="legal") | .name)' assets/data.json > assets/pioneer.json
-jq -s -c 'map(.[] | select(.legalities.modern=="legal") | .name)' assets/data.json > assets/modern.json
-jq -s -c 'map(.[] | select(.legalities.legacy=="legal") | .name)' assets/data.json > assets/legacy.json
-jq -s -c 'map(.[] | select(.legalities.vintage=="legal") | .name)' assets/data.json > assets/vintage.json
-rm -fv assets/data.json
+jq -s 'map(.[] | select(.legalities.standard=="legal") | .name)' assets/data.json > assets/standard.json
+jq -s 'map(.[] | select(.legalities.pauper=="legal") | .name)' assets/data.json > assets/pauper.json
+jq -s 'map(.[] | select(.legalities.pioneer=="legal") | .name)' assets/data.json > assets/pioneer.json
+jq -s 'map(.[] | select(.legalities.modern=="legal") | .name)' assets/data.json > assets/modern.json
+jq -s 'map(.[] | select(.legalities.legacy=="legal") | .name)' assets/data.json > assets/legacy.json
+jq -s 'map(.[] | select(.legalities.vintage=="legal") | .name)' assets/data.json > assets/vintage.json
+rm -f ./data.json
